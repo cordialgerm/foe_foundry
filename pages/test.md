3155fd4cc51a3aaf36fc3cd2c63fd769c4bd4e72
Evan Rash <evanrash@gmail.com>
2023-09-30 14:52:54 -0700

    test

---
+++ pages/test.md
@@ -0,0 +1,6 @@
+# TEST MARKDOWN FILE
+
+Hello World
+
+
+![Test](../screenshots/Brute_Celestial_Bruiser_0.png)
--- tests/__main__.py
+++ tests/__main__.py
@@ -1,17 +1,21 @@
 import subprocess
 import sys
 from fnmatch import fnmatch
+from io import BytesIO
 from pathlib import Path
 
 import click
 import numpy as np
 import pytest
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from .filtering import filter_examples
 
 
 @click.command()
-@click.option("--display", default=5, help="displays a random sample of the specified size")
+@click.option("--display", default=1, help="displays a random sample of the specified size")
 @click.option(
     "--name",
     default="",
@@ -52,6 +56,7 @@ def run_tests(display: int, feature: str, name: str):
         selections = rng.choice(a=np.array(matches, dtype=object), size=size, replace=False)
         for selection in selections:
             open_statblock(selection)
+            screenshot(selection)
 
     sys.exit(retcode)
 
@@ -67,6 +72,37 @@ def open_statblock(path: Path):
     rel = path.relative_to(cwd)
     print(f"Displaying {rel}...")
     subprocess.run(["start", str(rel)], shell=True)
+
+
+def screenshot(path: Path):
+    screenshots_dir = Path(__file__).parent.parent / "screenshots"
+    screenshots_dir.mkdir(exist_ok=True)
+    screenshot_path = screenshots_dir / f"{path.stem}.png"
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.get(path.as_uri())
+
+    element = driver.find_element(by=By.CLASS_NAME, value="stat-block")
+    location = element.location_once_scrolled_into_view
+    size = element.size
+
+    bytes = driver.get_screenshot_as_png()
+    driver.quit()
+
+    img = Image.open(BytesIO(bytes))
+    left = location["x"]
+    top = location["y"]
+    right = (
+        location["x"] + 1.25 * size["width"] + 10
+    )  # main monitor messes this up... it has 125% scaling
+    bottom = (
+        location["y"] + 1.25 * size["height"] + 10
+    )  # main monitor messes this up... it has 125% scaling
+    box = (left, top, right, bottom)
+    img2 = img.crop(box)
+    img2.save(screenshot_path, format="png")
 
 
 if __name__ == "__main__":
