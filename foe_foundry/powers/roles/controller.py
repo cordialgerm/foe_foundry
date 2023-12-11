3c6785baab8c3e3c468b235ef9d337d20df12955
Evan Rash <cordialgerm87@gmail.com>
2023-12-11 00:34:46 -0800

    controller spells

--- foe_foundry/powers/roles/controller.py
+++ foe_foundry/powers/roles/controller.py
@@ -1,17 +1,25 @@
+import math
 from datetime import datetime
 from typing import List
 
+from num2words import num2words
+
 from ...attack_template import natural, spell, weapon
 from ...creature_types import CreatureType
-from ...damage import AttackType, DamageType
+from ...damage import AttackType, DamageType, conditions
 from ...die import Die
 from ...features import ActionType, Feature
 from ...role_types import MonsterRole
 from ...statblocks import BaseStatblock
-from ..power import Power, PowerType, PowerWithStandardScoring
-
-# TODO - Controlling Spells
-# TODO - Advanced Controlling Spells
+from ..power import (
+    HIGH_POWER,
+    LOW_POWER,
+    MEDIUM_POWER,
+    Power,
+    PowerType,
+    PowerWithStandardScoring,
+)
+from ..spell import Spell, SpellPower
 
 
 class _PacifyingTouch(PowerWithStandardScoring):
@@ -115,6 +123,7 @@ class _HeartTremors(PowerWithStandardScoring):
             power_type=PowerType.Role,
             create_date=datetime(2023, 11, 29),
             theme="controller",
+            source="FoeFoundry",
             score_args=dict(
                 require_roles=MonsterRole.Controller,
                 attack_names=["-", weapon.Staff, natural.Slam, spell.Shock],
@@ -126,7 +135,6 @@ class _HeartTremors(PowerWithStandardScoring):
         dmg = stats.target_value(2.5, force_die=Die.d10)
         feature = Feature(
             name="Heart Tremors",
-            source="FoeFoundry",
             action=ActionType.BonusAction,
             recharge=5,
             description=f"Immediately after {stats.selfref} hits with an attack, it can force the target to make a DC {dc} Constitution save. On a failure, \
@@ -136,14 +144,229 @@ class _HeartTremors(PowerWithStandardScoring):
         return [feature]
 
 
-# Enemies Abound cast on you. If you hit someone. Chance to spread to them - controller caster - controller caste
-# Nervefire - if you take an action other than dodge, suffer X psychic damage. Save ends at end of turn - controller caster
-# Bonerot - bones begin to turn to dust. Necrotic damage and fatigue - controller poison, necrotic
+class _UnhingedParanoia(PowerWithStandardScoring):
+    def __init__(self):
+        super().__init__(
+            name="Unhinged Paranoia",
+            power_type=PowerType.Role,
+            create_date=datetime(2023, 12, 10),
+            theme="controller",
+            source="FoeFoundry",
+            score_args=dict(
+                require_roles=MonsterRole.Controller,
+                require_damage=DamageType.Psychic,
+                require_damage_exact_match=True,
+            ),
+        )
+
+    def generate_features(self, stats: BaseStatblock) -> List[Feature]:
+        dc = stats.difficulty_class
+        feature = Feature(
+            name="Unhinged Paranoia",
+            action=ActionType.Action,
+            replaces_multiattack=1,
+            description=f"{stats.selfref.capitalize()} attempts to destabilize the mind of a creature it can see within 60 feet. \
+                    That creature must make a DC {dc} Intelligence saving throw. On a failure, the creature becomes unable to distinguish friend from foe. \
+                    At the start of each of the creature's turns, it must roll a die to determine its behavior. \
+                    On an odd roll, the creature considers its former allies to be its enemies and must act accordingly during this turn. \
+                    The condition ends when the creature causes damage to one of its former allies in this manner.",
+        )
+        return [feature]
+
+
+class _Nervefire(PowerWithStandardScoring):
+    def __init__(self):
+        super().__init__(
+            name="Nervefire",
+            power_type=PowerType.Role,
+            create_date=datetime(2023, 12, 10),
+            theme="controller",
+            source="FoeFoundry",
+            score_args=dict(
+                require_roles=MonsterRole.Controller,
+                require_damage=DamageType.Poison,
+                require_damage_exact_match=True,
+            ),
+        )
+
+    def generate_features(self, stats: BaseStatblock) -> List[Feature]:
+        dc = stats.difficulty_class
+        dmg = stats.target_value(0.75, force_die=Die.d6)
+        feature = Feature(
+            name="Nervefire",
+            action=ActionType.BonusAction,
+            recharge=5,
+            description=f"Immediately after hitting a creature with an attack, {stats.selfref} can force the target to make a DC {dc} Constitution saving throw. \
+                On a failure, the target is **Poisoned** (save ends at end of turn). While poisoned in this way, the target takes {dmg.description} psychic damage \
+                whenever it takes an action other than the Dodge action.",
+        )
+        return [feature]
+
+
+class _TiringAttack(PowerWithStandardScoring):
+    def __init__(self):
+        super().__init__(
+            name="Tiring Attack",
+            power_type=PowerType.Role,
+            create_date=datetime(2023, 12, 10),
+            theme="controller",
+            source="FoeFoundry",
+            power_level=HIGH_POWER,
+            score_args=dict(
+                require_roles=MonsterRole.Controller,
+                require_damage=DamageType.Necrotic,
+                require_damage_exact_match=True,
+            ),
+        )
+
+    def generate_features(self, stats: BaseStatblock) -> List[Feature]:
+        dc = stats.difficulty_class
+        fatigue = conditions.Fatigue()
+        feature = Feature(
+            name="Tiring Attack",
+            action=ActionType.Feature,
+            modifies_attack=True,
+            description=f"On a hit, the target must make a DC {dc} Constitution saving throw. \
+                On a failure, the target's bones begin to turn to dust and it gains a level of {fatigue.caption}. {fatigue.description_3rd}",
+        )
+        return [feature]
+
+
+class _ControllingSpellPower(SpellPower):
+    def __init__(
+        self,
+        spell: Spell,
+    ):
+        super().__init__(
+            spell=spell,
+            power_type=PowerType.Role,
+            create_date=datetime(2023, 12, 10),
+            theme="controller",
+        )
+
+
+def _ControllingSpells() -> List[Power]:
+    spells = [
+        Spell(
+            name="Blindness/Deafness",
+            level=2,
+            upcast=True,
+            score_args=dict(
+                require_damage=[DamageType.Necrotic, DamageType.Poison],
+            ),
+        ),
+        Spell(
+            name="Command",
+            level=1,
+            upcast=True,
+            score_args=dict(
+                require_types=[
+                    CreatureType.Humanoid,
+                    CreatureType.Fey,
+                    CreatureType.Fiend,
+                    CreatureType.Celestial,
+                ],
+            ),
+        ),
+        Spell(
+            name="Entangle",
+            level=1,
+            upcast=False,
+            score_args=dict(
+                require_types=[CreatureType.Plant, CreatureType.Fey, CreatureType.Humanoid]
+            ),
+        ),
+        Spell(
+            name="Grease",
+            level=1,
+            action_type=ActionType.BonusAction,
+            upcast=False,
+            power_level=LOW_POWER,
+            score_args=dict(require_types=CreatureType.Humanoid),
+        ),
+        Spell(
+            name="Gust of Wind",
+            level=2,
+            upcast=False,
+            power_level=LOW_POWER,
+            score_args=dict(require_damage=[DamageType.Lightning, DamageType.Thunder]),
+        ),
+        Spell(
+            name="Hideous Laughter",
+            level=1,
+            upcast=False,
+            power_level=LOW_POWER,
+            score_args=dict(require_damage=DamageType.Psychic),
+        ),
+        Spell(name="Hold Person", level=2, upcast=True, power_level=HIGH_POWER),
+        Spell(
+            name="Levitate",
+            level=2,
+            upcast=False,
+            power_level=LOW_POWER,
+            score_args=dict(require_types=[CreatureType.Elemental, CreatureType.Humanoid]),
+        ),
+        Spell(
+            name="Sleet Storm",
+            level=3,
+            upcast=False,
+            score_args=dict(require_damage=DamageType.Cold),
+        ),
+        Spell(name="Silence", level=2, upcast=False, power_level=LOW_POWER),
+        Spell(
+            name="Suggestion",
+            level=2,
+            upcast=False,
+            score_args=dict(
+                require_types=[
+                    CreatureType.Humanoid,
+                    CreatureType.Fiend,
+                    CreatureType.Fey,
+                    CreatureType.Monstrosity,
+                ]
+            ),
+        ),
+        Spell(
+            name="Web",
+            level=2,
+            upcast=False,
+            score_args=dict(require_types=[CreatureType.Monstrosity, CreatureType.Humanoid]),
+        ),
+        Spell(
+            name="Slow",
+            level=3,
+            upcast=False,
+            score_args=dict(
+                require_types=[
+                    CreatureType.Humanoid,
+                ]
+            ),
+        ),
+        Spell(
+            name="Fog Cloud",
+            level=1,
+            upcast=True,
+            power_level=LOW_POWER,
+        ),
+    ]
+
+    return [_ControllingSpellPower(spell) for spell in spells]
 
 
 Eyebite: Power = _Eyebite()
 HeartTremors: Power = _HeartTremors()
+NerveFire: Power = _Nervefire()
 PacifyingTouch: Power = _PacifyingTouch()
+TiringAttack: Power = _TiringAttack()
 TongueTwister: Power = _TongueTwister()
+UnhingedParanoia: Power = _UnhingedParanoia()
+ControllingSpells: List[Power] = _ControllingSpells()
 
-ControllerPowers: List[Power] = [Eyebite, HeartTremors, PacifyingTouch, TongueTwister]
+ControllerPowers: List[Power] = [
+    Eyebite,
+    HeartTremors,
+    NerveFire,
+    PacifyingTouch,
+    TongueTwister,
+    UnhingedParanoia,
+] + ControllingSpells
--- foe_foundry/powers/themed/magic.py
+++ foe_foundry/powers/themed/magic.py
@@ -1,1 +1,200 @@
 # TODO
+
+from datetime import datetime
+from typing import List
+
+from ...attack_template import weapon
+from ...attributes import Stats
+from ...creature_types import CreatureType
+from ...damage import AttackType, DamageType, conditions
+from ...die import Die
+from ...features import ActionType, Feature
+from ...role_types import MonsterRole
+from ...statblocks import BaseStatblock
+from ..power import HIGH_POWER, MEDIUM_POWER, Power, PowerType, PowerWithStandardScoring
+from ..spell import Spell, SpellPower
+
+
+def humanoid_is_spellcaster(c: BaseStatblock) -> bool:
+    if c.creature_type == CreatureType.Humanoid:
+        return c.attack_type in AttackType.AllSpell()
+    else:
+        return True
+
+
+class _MagicPower(SpellPower):
+    def __init__(self, spell: Spell):
+        super().__init__(
+            spell, power_type=PowerType.Theme, theme="magic", create_date=datetime(2023, 12, 10)
+        )
+
+
+class _DispelMagic(_MagicPower):
+    def __init__(self):
+        super().__init__(
+            spell=Spell(
+                name="Dispel Magic",
+                level=3,
+                upcast=True,
+                score_args=dict(
+                    require_types=[
+                        CreatureType.Fey,
+                        CreatureType.Fiend,
+                        CreatureType.Celestial,
+                        CreatureType.Humanoid,
+                    ],
+                    require_callback=humanoid_is_spellcaster,
+                    require_cr=4,
+                    bonus_roles=[
+                        MonsterRole.Defender,
+                        MonsterRole.Leader,
+                        MonsterRole.Controller,
+                    ],
+                ),
+            )
+        )
+
+
+class _Disintigrate(_MagicPower):
+    def __init__(self):
+        super().__init__(
+            spell=Spell(
+                name="Disintigrate",
+                level=6,
+                upcast=False,
+                power_level=HIGH_POWER,
+                score_args=dict(
+                    require_types=[
+                        CreatureType.Humanoid,
+                    ],
+                    require_callback=humanoid_is_spellcaster,
+                    require_damage=[DamageType.Force, DamageType.Necrotic],
+                    require_cr=7,
+                ),
+            )
+        )
+
+
+class _FingerOfDeath(_MagicPower):
+    def __init__(self):
+        super().__init__(
+            spell=Spell(
+                name="Finger of Death",
+                level=7,
+                upcast=False,
+                power_level=HIGH_POWER,
+                score_args=dict(
+                    require_types=[CreatureType.Humanoid, CreatureType.Undead],
+                    require_callback=humanoid_is_spellcaster,
+                    require_damage=[DamageType.Necrotic],
+                    require_cr=8,
+                ),
+            )
+        )
+
+
+class _ChainLightning(_MagicPower):
+    def __init__(self):
+        super().__init__(
+            spell=Spell(
+                name="Chain Lightning",
+                level=6,
+                upcast=False,
+                power_level=HIGH_POWER,
+                score_args=dict(
+                    require_types=[
+                        CreatureType.Humanoid,
+                        CreatureType.Elemental,
+                        CreatureType.Giant,
+                    ],
+                    require_callback=humanoid_is_spellcaster,
+                    require_damage=[DamageType.Lightning, DamageType.Thunder],
+                    require_cr=7,
+                ),
+            )
+        )
+
+
+class _Banishment(_MagicPower):
+    def __init__(self):
+        super().__init__(
+            spell=Spell(
+                name="Banishment",
+                level=4,
+                upcast=True,
+                power_level=HIGH_POWER,
+                score_args=dict(
+                    require_types=[
+                        CreatureType.Humanoid,
+                    ],
+                    bonus_roles=MonsterRole.Controller,
+                    require_damage=[DamageType.Radiant],
+                    require_callback=humanoid_is_spellcaster,
+                    require_cr=5,
+                ),
+            )
+        )
+
+
+class _Fireball(_MagicPower):
+    def __init__(self):
+        super().__init__(
+            spell=Spell(
+                name="Fireball",
+                level=3,
+                upcast=True,
+                power_level=HIGH_POWER,
+                score_args=dict(
+                    require_types=[
+                        CreatureType.Humanoid,
+                        CreatureType.Fiend,
+                        CreatureType.Elemental,
+                    ],
+                    require_damage=[DamageType.Fire],
+                    require_callback=humanoid_is_spellcaster,
+                    require_cr=5,
+                ),
+            )
+        )
+
+
+class _LightningBolt(_MagicPower):
+    def __init__(self):
+        super().__init__(
+            spell=Spell(
+                name="Lightning Bolt",
+                level=3,
+                upcast=True,
+                power_level=HIGH_POWER,
+                score_args=dict(
+                    require_types=[
+                        CreatureType.Humanoid,
+                        CreatureType.Elemental,
+                        CreatureType.Giant,
+                    ],
+                    require_damage=[DamageType.Lightning, DamageType.Thunder],
+                    require_callback=humanoid_is_spellcaster,
+                    require_cr=5,
+                ),
+            )
+        )
+
+
+Banishment: Power = _Banishment()
+ChainLightning: Power = _ChainLightning()
+DispelMagic: Power = _DispelMagic()
+Disintigrate: Power = _Disintigrate()
+FingerOfDeath: Power = _FingerOfDeath()
+Fireball: Power = _Fireball()
+LightningBolt: Power = _LightningBolt()
+
+
+MagicPowers: List[Power] = [
+    Banishment,
+    ChainLightning,
+    DispelMagic,
+    Disintigrate,
+    FingerOfDeath,
+    Fireball,
+    LightningBolt,
+]
