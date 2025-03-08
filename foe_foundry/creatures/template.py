01fe3ad98c61130a853eae7afd2598bd26caae92
Evan Rash <erash@gofundme.com>
2025-03-07 20:24:36 -0800

    working on species

--- foe_foundry/creatures/template.py
+++ foe_foundry/creatures/template.py
@@ -70,18 +70,6 @@ class CreatureVariant:
 
 
 @dataclass(kw_only=True, frozen=True)
-class CreatureSpecies:
-    name: str
-    description: str
-
-    def alter_base_stats(self, stats: BaseStatblock) -> BaseStatblock:
-        return stats
-
-    def __hash__(self) -> int:
-        return hash(self.name)
-
-
-@dataclass(kw_only=True, frozen=True)
 class GenerationSettings:
     creature_name: str
     cr: float
