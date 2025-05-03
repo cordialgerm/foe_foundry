62006c79f53ebf573b98221d2c762602dfff47ce
Evan Rash <cordialgerm87@gmail.com>
2025-05-03 12:34:19 -0700

    villains

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -12,6 +12,7 @@ poetry install --sync
 
 # Set PYTHONPATH so that mkdocs can find the package in this repo
 export PYTHONPATH=.
+export SITE_URL=${SITE_URL:-http://127.0.0.1:8000/}
 
 # Build the site
 poetry run mkdocs build --clean
--- tests/test_md.py
+++ tests/test_md.py
@@ -1,8 +1,17 @@
+import dotenv
+
 from foe_foundry.markdown import markdown
 
 
 def test_markdown():
-    text = "This is a **Wight** and these are [[Zombies]] and this is a button $[[Lich]] and this is **NOTHING IMPORTANT** and this is a statblock ![[Bandit]]"
-
+    dotenv.load_dotenv()
+    text = "This is a **Wight** and these are [[Zombies]] and this is a button [[$Lich]] and this is **NOTHING IMPORTANT** and this is a statblock [[!Bandit]]"
     result = markdown(text)
     assert len(result.references) == 4
+
+
+def test_necromancer_primagus():
+    dotenv.load_dotenv()
+    text = "This is a **Necromancer Primagus** and this is [[$Necromancer Primagus]] and [[!Necromancer Primagus]]"
+    result = markdown(text)
+    assert len(result.references) == 3
