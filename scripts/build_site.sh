2e87afcf815625eb6ee454828ba8742c61073ab4
Evan Rash <cordialgerm87@gmail.com>
2025-05-18 19:52:11 -0700

    try again

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -7,18 +7,19 @@ set -e
 # Move back to the root of the repository
 cd "$(dirname "$0")/.."
 
+# if the repository is shallow, fetch the rest of the history
+# this is needed for the blogging plugin, which relies on git history
+if git rev-parse --is-shallow-repository; then
+  echo "Fetching the rest of the git history..."
+  git fetch --unshallow
+fi
+
 # Make sure the venv is in sync with the lock file
 poetry install --sync
 
 # Set PYTHONPATH so that mkdocs can find the package in this repo
 export PYTHONPATH=.
 export SITE_URL=${SITE_URL:-http://127.0.0.1:8080/}
-
-# if the repository is shallow, fetch the rest of the history
-# this is needed for the blogging plugin, which relies on git history
-if git rev-parse --is-shallow-repository; then
-  git fetch --unshallow
-fi
 
 # Build the static content
 poetry run mkdocs build --clean
