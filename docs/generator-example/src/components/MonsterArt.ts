b8f1d1f1fe2c790fac03687f38e00748cfbfda91
Evan Rash <cordialgerm87@gmail.com>
2025-07-15 22:57:07 -0700

    progress

--- docs/generator-example/src/components/MonsterArt.ts
+++ docs/generator-example/src/components/MonsterArt.ts
@@ -10,14 +10,11 @@ export class MonsterArt extends LitElement {
   @property({ type: String, attribute: 'background-image' })
   backgroundImage: string = '';
 
+  @property({ type: String, attribute: 'background-color' })
+  backgroundColor: string = 'rgba(255, 255, 255, 0.55)';
+
   @property({ type: String, attribute: 'image-mode' })
   imageMode: 'contain' | 'cover' = 'contain';
-
-  @property({ type: Boolean, attribute: 'blend-mode' })
-  blendMode: boolean = false;
-
-  @property({ type: Boolean })
-  faded: boolean = false;
 
   static styles = css`
     :host {
@@ -61,6 +58,7 @@ export class MonsterArt extends LitElement {
       z-index: 2;
       object-fit: contain;
       object-position: center;
+      mix-blend-mode: multiply;
     }
 
     .card-image.contain {
@@ -72,14 +70,6 @@ export class MonsterArt extends LitElement {
       object-fit: cover;
       object-position: center;
     }
-
-    .card-image.blend {
-      mix-blend-mode: multiply;
-    }
-
-    .card-image.faded {
-      opacity: 0.6;
-    }
   `;
 
   private get imageClasses(): string {
@@ -87,18 +77,12 @@ export class MonsterArt extends LitElement {
     if (this.imageMode) {
       classes.push(this.imageMode);
     }
-    if (this.blendMode) {
-      classes.push('blend');
-    }
-    if (this.faded) {
-      classes.push('faded');
-    }
     return classes.join(' ');
   }
 
   private get containerStyle(): string {
     if (this.backgroundImage) {
-      return `background-image: url('${this.backgroundImage}')`;
+      return `background-image: url('${this.backgroundImage}'); background-color: ${this.backgroundColor}; background-blend-mode: lighten;`;
     }
     return '';
   }
