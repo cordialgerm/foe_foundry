89a0d958bf492d13384f3448f99467f19cff25f8
Evan Rash <cordialgerm87@gmail.com>
2025-08-01 15:55:54 -0700

    fix cicd

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -9,6 +9,9 @@ cd "$(dirname "$0")/.."
 # Make sure the venv is in sync with the lock file
 poetry install --sync
 
+# Make sure the node modules are in sync with the lock file
+npm ci
+
 # Set PYTHONPATH so that mkdocs can find the package in this repo
 export PYTHONPATH=.
 export SITE_URL=${SITE_URL:-http://127.0.0.1:8080/}
